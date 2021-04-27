51c2760c8467a17863d2994bc431b4ba24bbd26e
marmon179 <marmon179@gmail.com>
2021-04-27 21:40:34 +0300

    task20

---
+++ src/codewars/task20.js
@@ -0,0 +1,8 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+//
+//     String will never be empty and you do not need to account for different data types.
+
+const findShort = s => s.split(' ').sort((a, b) => a.length - b.length)[0].length;
+
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"))
