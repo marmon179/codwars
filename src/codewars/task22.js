c58814572f8df969185dd9d0bde21bb653d20af4
marmon179 <marmon179@gmail.com>
2021-05-27 12:39:17 +0300

    task22

---
+++ src/codewars/task22.js
@@ -0,0 +1,19 @@
+// let i = 0
+// while (i < 3) {
+//     console.log(i)
+//     i++
+// };
+//
+// for (let i = 0; i < 4; i++) {
+//     console.log(i)
+// }
+// for (let i = 0; i < 10; i++) {
+//     if (i % 2 === 0) continue
+//     console.log(i)
+// }
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
